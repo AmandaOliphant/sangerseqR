51400633aa98a76c35f584f7294e45b8bfcf60b2
edvallog <logan.edvalson@gmail.com>
2019-09-21 11:35:15 -0600

    Returned Chromatogram to working by excluding unecported file

--- R/sangerseqmethods.R
+++ R/sangerseqmethods.R
@@ -125,149 +125,130 @@ setMethod("chromatogram", "scf",
 
 #' @rdname chromatogram
 setMethod("chromatogram", "sangerseq", 
-  function(obj, trim5=0, trim3=0, 
-           showcalls=c("primary", "secondary", "both", "none"), 
-           width=100, height=2, cex.mtext=1, cex.base=1, ylim=3, 
-           filename=NULL, showtrim=FALSE, showhets=TRUE) {
-    #need to check if argument is valid and throw up error if not. must be part of showcalls vector
-    originalpar <- par(no.readonly=TRUE) #parameters for graphics. saves what they were to set them back at end
-    showcalls <- showcalls[1] #grab the best value (or first) vector of all possible arguments that are allowed see 123
-    traces <- obj@traceMatrix #values of each laser shot acgt
-    basecalls1 <- unlist(strsplit(toString(obj@primarySeq), "")) #creates vector out of basically long word a,c,g,t from acgt
-    basecalls2 <- unlist(strsplit(toString(obj@secondarySeq), ""))
-    averagePosition <- rowMeans(obj@peakPosMatrix, na.rm=TRUE)#peakposmatrix (takes average of peak positions of all bases)
-    
-#    basecalls1 <- trimBasecalls(basecalls1, showtrim, trim5, trim3, averagePosition)
-#    basecalls2 <- trimBasecalls(basecalls2, showtrim, trim5, trim3, averagePosition) #trims base calls
-    
-    if(showtrim == FALSE) {
-      averagePosition <- averagePosition[(1 + trim5):(length(averagePosition) - trim3)] #move into trimBaseCalls and call function. 
-    }
-    
-    indexes <- 1:length(basecalls1)
-    trimmed <- indexes <= trim5 | indexes > (length(basecalls1) - trim3) # all false if not trimmed
-    
-    if (!is.null(trim3)) {
-      traces <- traces[1:(min(max(averagePosition, na.rm=TRUE) + 10, 
-                              nrow(traces))), ]
-    }
-    if (!is.null(trim5)) {
-      offset <- max(c(1, averagePosition[1] - 10))
-      traces <- traces[offset:nrow(traces),]
-      averagePosition <- averagePosition - (offset-1)
-    }
-    
-    #midp starts startHets ends, etc. making the shaded boxes; make this its own function
-    #only the hets are shaded; without calling makeBaseCalls, it's all shaded
-    
-    #toggle between shading hets and showing quality scores (bar graph), maybe quality scores could be the default
-    #basically the shading would be different heights depending on how confident it is (do the shading behind)
-    #scale the quality scores to match the height of the graph
-    #maybe a y-axis on the right side to show the scale of the quality (find a way to have 2 y-axes)
-    
-    maxSignal <- apply(traces, 1, max)
-    ylims <- c(0, quantile(maxSignal, .75)+ylim*IQR(maxSignal))           
-    p <- c(0, averagePosition, nrow(traces))
-    midp <- diff(p)/2
-    starts <- averagePosition - midp[1:(length(midp)-1)]
-    startHets <- starts
-    startHets[basecalls1 == basecalls2] <- NA
-    ends <- averagePosition + midp[2:(length(midp))]
-    endHets <- ends
-    endHets[basecalls1 == basecalls2] <- NA
-    startTrims <- starts
-    startTrims[!trimmed] <- NA
-    endTrims <- ends
-    endTrims[!trimmed] <- NA
-    
-    #obj <- chromatogramColor(obj, colorTranslate, colorVector1, colorVector2, basecalls1, basecalls2)
-    colorTranslate <- c(A="green", C="blue", G="black", T="red")
-    colorVector1 <- unname(colorTranslate[basecalls1])
-    colorVector1[is.na(colorVector1)] <- "purple"
-    colorVector2 <- unname(colorTranslate[basecalls2])
-    colorVector2[is.na(colorVector2)] <- "purple"
-    
-    valuesPerBase <- nrow(traces)/length(basecalls1)
-    traceWidth <- width*valuesPerBase
-    breaks <- seq(1,nrow(traces), by=traceWidth)
-    numPlots <- length(breaks)
-    
-    if(!is.null(filename)) {
-      pdf(filename, width=8.5, height=height*numPlots)
-    }
-    par(mar=c(2,2,2,1), mfrow=c(numPlots, 1))
-    basecallwarning1 = 0
-    basecallwarning2 = 0
-    j = 1
-    
-    #try to see if there's a way to not use this loop
-    for(i in breaks) {
-      
-      range <- averagePosition >= i & averagePosition < (i+traceWidth)
-      startHet <- startHets[range] - traceWidth*(j-1)
-      startHet[startHet < 0] <- 0
-      endHet <- endHets[range] - traceWidth*(j-1)
-      endHet[endHet > traceWidth] <- traceWidth
-      lab1 <- basecalls1[range]
-      lab2 <- basecalls2[range]
-      pos <- averagePosition[range] - traceWidth*(j-1)
-      colors1 <- colorVector1[range]
-      colors2 <- colorVector2[range]
-      startTrim <- startTrims[range] - traceWidth*(j-1)
-      endTrim <- endTrims[range] - traceWidth*(j-1)
-      plotRange <- i:min(i+traceWidth, nrow(traces))
-      plot(traces[plotRange,1], type='n', ylim=ylims, ylab="", xaxt="n", 
-           bty="n", xlab="", yaxt="n", , xlim=c(1,traceWidth))
-      if (showhets==TRUE) {
-        rect(startHet, 0, endHet, ylims[2], col='#D5E3F7', border='#D5E3F7')
-      }
-      if (showtrim==TRUE) {
-        rect(startTrim, 0, endTrim, ylims[2], col='red', border='transparent', 
-             density=15)
-      }
-      lines(traces[plotRange,1], col="green")
-      lines(traces[plotRange,2], col="blue")
-      lines(traces[plotRange,3], col="black")
-      lines(traces[plotRange,4], col="red")
-      mtext(as.character(which(range)[1]), side=2, line=0, cex=cex.mtext)
-      
-      #obj <- alignBaseCalls(obj, showcalls, basecallwarning1, basecallwarning2)
-
-      for(k in 1:length(lab1)) {
-        if (showcalls=="primary" | showcalls=="both") {
-          if (is.na(basecalls1[1]) & basecallwarning1==0) {
-            warning("Primary basecalls missing")
-            basecallwarning1 = 1
-          } 
-          else if (length(lab1) > 0) {   
-            axis(side=3, at=pos[k], labels=lab1[k], col.axis=colors1[k], 
-                 family="mono", cex=cex.base, line=ifelse(showcalls=="both", 0, 
-                                                          -1), tick=FALSE)
+          function(obj, trim5=0, trim3=0, 
+                   showcalls=c("primary", "secondary", "both", "none"), 
+                   width=100, height=2, cex.mtext=1, cex.base=1, ylim=3, 
+                   filename=NULL, showtrim=FALSE, showhets=TRUE) {
+            originalpar <- par(no.readonly=TRUE)
+            showcalls <- showcalls[1]
+            traces <- obj@traceMatrix
+            basecalls1 <- unlist(strsplit(toString(obj@primarySeq), ""))
+            basecalls2 <- unlist(strsplit(toString(obj@secondarySeq), ""))
+            aveposition <- rowMeans(obj@peakPosMatrix, na.rm=TRUE)
+            basecalls1 <- basecalls1[1:length(aveposition)] 
+            basecalls2 <- basecalls2[1:length(aveposition)] 
+            if(showtrim == FALSE) {
+              if(trim5+trim3 > length(basecalls1)) basecalls1 <- ""
+              else basecalls1 <- basecalls1[(1 + trim5):(length(basecalls1) - trim3)]
+              if(trim5+trim3 > length(basecalls2)) basecalls2 <- ""
+              else basecalls2 <- basecalls2[(1 + trim5):(length(basecalls2) - trim3)]
+              aveposition <- aveposition[(1 + trim5):(length(aveposition) - trim3)] 
+            }
+            indexes <- 1:length(basecalls1)
+            trimmed <- indexes <= trim5 | indexes > (length(basecalls1) - trim3) # all 
+            #false if not trimmed
+            if (!is.null(trim3)) {
+              traces <- traces[1:(min(max(aveposition, na.rm=TRUE) + 10, 
+                                      nrow(traces))), ]
+            }
+            if (!is.null(trim5)) {
+              offset <- max(c(1, aveposition[1] - 10))
+              traces <- traces[offset:nrow(traces),]
+              aveposition <- aveposition - (offset-1)
+            }
+            maxsignal <- apply(traces, 1, max)
+            ylims <- c(0, quantile(maxsignal, .75)+ylim*IQR(maxsignal))           
+            p <- c(0, aveposition, nrow(traces))
+            midp <- diff(p)/2
+            starts <- aveposition - midp[1:(length(midp)-1)]
+            starthets <- starts
+            starthets[basecalls1 == basecalls2] <- NA
+            ends <- aveposition + midp[2:(length(midp))]
+            endhets <- ends
+            endhets[basecalls1 == basecalls2] <- NA
+            starttrims <- starts
+            starttrims[!trimmed] <- NA
+            endtrims <- ends
+            endtrims[!trimmed] <- NA
+            
+            colortranslate <- c(A="green", C="blue", G="black", T="red")
+            colorvector1 <- unname(colortranslate[basecalls1])
+            colorvector1[is.na(colorvector1)] <- "purple"
+            colorvector2 <- unname(colortranslate[basecalls2])
+            colorvector2[is.na(colorvector2)] <- "purple"
+            
+            valuesperbase <- nrow(traces)/length(basecalls1)
+            tracewidth <- width*valuesperbase
+            breaks <- seq(1,nrow(traces), by=tracewidth)
+            numplots <- length(breaks)
+            if(!is.null(filename)) pdf(filename, width=8.5, height=height*numplots) 
+            par(mar=c(2,2,2,1), mfrow=c(numplots, 1))
+            basecallwarning1 = 0
+            basecallwarning2 = 0
+            j = 1
+            
+            for(i in breaks) {
+              range <- aveposition >= i & aveposition < (i+tracewidth)
+              starthet <- starthets[range] - tracewidth*(j-1)
+              starthet[starthet < 0] <- 0
+              endhet <- endhets[range] - tracewidth*(j-1)
+              endhet[endhet > tracewidth] <- tracewidth
+              lab1 <- basecalls1[range]
+              lab2 <- basecalls2[range]
+              pos <- aveposition[range] - tracewidth*(j-1)
+              colors1 <- colorvector1[range]
+              colors2 <- colorvector2[range]
+              starttrim <- starttrims[range] - tracewidth*(j-1)
+              endtrim <- endtrims[range] - tracewidth*(j-1)
+              plotrange <- i:min(i+tracewidth, nrow(traces))
+              plot(traces[plotrange,1], type='n', ylim=ylims, ylab="", xaxt="n", 
+                   bty="n", xlab="", yaxt="n", , xlim=c(1,tracewidth))
+              if (showhets==TRUE) {
+                rect(starthet, 0, endhet, ylims[2], col='#D5E3F7', border='#D5E3F7')
+              }
+              if (showtrim==TRUE) {
+                rect(starttrim, 0, endtrim, ylims[2], col='red', border='transparent', 
+                     density=15)
+              }
+              lines(traces[plotrange,1], col="green")
+              lines(traces[plotrange,2], col="blue")
+              lines(traces[plotrange,3], col="black")
+              lines(traces[plotrange,4], col="red")
+              mtext(as.character(which(range)[1]), side=2, line=0, cex=cex.mtext)
+              
+              for(k in 1:length(lab1)) {
+                if (showcalls=="primary" | showcalls=="both") {
+                  if (is.na(basecalls1[1]) & basecallwarning1==0) {
+                    warning("Primary basecalls missing")
+                    basecallwarning1 = 1
+                  } 
+                  else if (length(lab1) > 0) {   
+                    axis(side=3, at=pos[k], labels=lab1[k], col.axis=colors1[k], 
+                         family="mono", cex=cex.base, line=ifelse(showcalls=="both", 0, 
+                                                                  -1), tick=FALSE)
+                  }
+                }
+                if (showcalls=="secondary" | showcalls=="both") {
+                  if (is.na(basecalls2[1]) & basecallwarning2 == 0) {
+                    warning("Secondary basecalls missing")
+                    basecallwarning2 = 1
+                  } 
+                  else if (length(lab2) > 0) { 
+                    axis(side=3, at=pos[k], labels=lab2[k], col.axis=colors2[k], 
+                         family="mono", cex=cex.base, line=-1, tick=FALSE)
+                  }
+                }
+              }
+              j = j + 1
+            }
+            if(!is.null(filename)) {
+              dev.off()
+              cat(paste("Chromatogram saved to", filename, 
+                        "in the current working directory"))
+            }
+            else par(originalpar)
           }
-        }
-        if (showcalls=="secondary" | showcalls=="both") {
-          if (is.na(basecalls2[1]) & basecallwarning2 == 0) {
-            warning("Secondary basecalls missing")
-            basecallwarning2 = 1
-          } 
-          else if (length(lab2) > 0) { 
-            axis(side=3, at=pos[k], labels=lab2[k], col.axis=colors2[k], 
-                 family="mono", cex=cex.base, line=-1, tick=FALSE)
-          }
-        }
-      }
-      j = j + 1
-    }
-    
-    if(!is.null(filename)) {
-      dev.off()
-      cat(paste("Chromatogram saved to", filename, 
-                "in the current working directory"))
-    }
-    else par(originalpar)
-  }
 )
-
 
 
 #' @rdname setAllelePhase
